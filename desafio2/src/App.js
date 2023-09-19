f30031ce0572ca6ae80cd21d0aa783679a58befe
Valentina <vabusto@escuelasproa.edu.ar>
2023-09-19 19:42:33 -0300

    flechas funcionales

--- desafio2/src/App.js
+++ desafio2/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import { useState } from 'react';
+import flechad from './img/flechaderecha.svg'
+import flechai from './img/flechai.svg'
+//logica
+export default function () {
+  const [index, setIndex] = useState(0);
+  const [index1, setIndex1] = useState(0);
+  const fecha = 
+  function menos() {
+    setIndex(index - 1);
+  }
+  function mas() {
+    setIndex(index + 1);
+  }
+  function restar() {
+    setIndex1(index1 - index);
+  }
+  function sumar() {
+    setIndex1(index1 + index);
+  }
+//estructura
   return (
     <div className="App">
+      <h1>Desafio 2</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='steps'>
+          <button onClick={menos}><img src={flechai}/></button>
+          <h1>{index}</h1>
+          <button onClick={mas}><img src={flechad}/></button>
+        </div>
+        <div className='change'>
+          <button onClick={restar}><img src={flechai}/></button>
+          <h2>{index1}</h2>
+          <button onClick={sumar}><img src={flechad}/></button>
+        </div> 
+        <h3>{fecha}</h3>
+        <img src='https://static.vecteezy.com/system/resources/previews/009/662/828/original/sun-sun-ray-icon-transparent-free-png.png' className="App-logo" alt="logo" />
       </header>
     </div>
   );
-}
+  }
 
-export default App;
+
