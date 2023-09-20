de713cef3ecba229e69937abab1e0e8f67d22aa1
Valentina <vabusto@escuelasproa.edu.ar>
2023-09-19 22:02:11 -0300

    intento de cambio de imagenes

--- desafio2/src/App.js
+++ desafio2/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import flechad from './img/flechaderecha.svg'
 import flechai from './img/flechai.svg'
+import moment from 'moment';
 //logica
 export default function () {
   const [index, setIndex] = useState(0);
   const [index1, setIndex1] = useState(0);
-  const fecha = 
+  const [fecha, setFecha] = useState(moment());
+  const [horaActual, setHoraActual] = useState(moment());
+  const urlImg = 'https://static.vecteezy.com/system/resources/previews/009/662/828/original/sun-sun-ray-icon-transparent-free-png.png';
+ 
   function menos() {
     setIndex(index - 1);
   }
@@ -15,15 +19,25 @@ export default function () {
   }
   function restar() {
     setIndex1(index1 - index);
+    fecha = moment(fecha).subtract(index, 'day');
   }
   function sumar() {
     setIndex1(index1 + index);
+    fecha = moment(fecha).add(index, 'day');
   }
+
+  if (horaActual.hours() < 7) {
+    urlImg = 'https://static.vecteezy.com/system/resources/previews/009/662/828/original/sun-sun-ray-icon-transparent-free-png.png';
+  } 
+  else {
+    urlImg = 'https://cdn-icons-png.flaticon.com/512/3026/3026346.png';
+  }
+
 //estructura
   return (
     <div className="App">
-      <h1>Desafio 2</h1>
       <header className="App-header">
+      <h1>Desafio 2</h1>
         <div className='steps'>
           <button onClick={menos}><img src={flechai}/></button>
           <h1>{index}</h1>
@@ -34,8 +48,9 @@ export default function () {
           <h2>{index1}</h2>
           <button onClick={sumar}><img src={flechad}/></button>
         </div> 
-        <h3>{fecha}</h3>
-        <img src='https://static.vecteezy.com/system/resources/previews/009/662/828/original/sun-sun-ray-icon-transparent-free-png.png' className="App-logo" alt="logo" />
+        <p>{fecha.format('DD-MM-YYYY')}</p>
+        <p>{horaActual}</p>
+        <img src={urlImg} className="App-logo" />
       </header>
     </div>
   );
