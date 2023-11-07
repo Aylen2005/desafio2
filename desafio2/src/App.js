f01f069c0f94cc223c774252f459f823aca7b521
Thiago46 <93220078+Aylen2005@users.noreply.github.com>
2023-11-07 13:56:10 -0300

    i dont know

--- desafio2/src/App.js
+++ desafio2/src/App.js
@@ -1,62 +1,31 @@
-import './App.css';
-import { useState } from 'react';
-import flechad from './img/flechaderecha.svg'
-import flechai from './img/flechai.svg'
-import moment from 'moment';
-//logica
-export default function () {
-  const [index, setIndex] = useState(0);
-  const [index1, setIndex1] = useState(0);
-  const [fecha, setFecha] = useState(moment());
-  const [horaActual, setHoraActual] = useState(moment());
-  let urlImg = 'https://static.vecteezy.com/system/resources/previews/009/662/828/original/sun-sun-ray-icon-transparent-free-png.png';
+import React, { useState } from 'react';
+import NameForm from './PopupForm';
 
-  function menos() {
-    setIndex(index - 1);
-  }
-  function mas() {
-    setIndex(index + 1);
-  }
-  function restar() {
-    setIndex1(index1 - index);
-    const newFecha = moment(fecha).subtract(index, 'day');
-    setFecha(newFecha);
-  }
-  function sumar() {
-    setIndex1(index1 + index);
-    const newFecha = moment(fecha).add(index, 'day');
-    setFecha(newFecha);
-  }
-  if (horaActual.hours() < 19) {
-    urlImg = 'https://static.vecteezy.com/system/resources/previews/009/662/828/original/sun-sun-ray-icon-transparent-free-png.png';
-  } 
-  else {
-    urlImg = 'https://cdn-icons-png.flaticon.com/512/3026/3026346.png';
-  }
+const App = () => {
+  const [mostrarFormulario, setMostrarFormulario] = useState(false);
+  const [nombreIngresado, setNombreIngresado] = useState('');
 
-  
+  const mostrarFormularioHandler = () => {
+    setMostrarFormulario(true);
+  };
 
-//estructura
+  const cerrarFormularioHandler = () => {
+    setMostrarFormulario(false);
+  };
+
+  const manejarFormulario = (nombre) => {
+    setNombreIngresado(nombre);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-      <h1>Desafio 2</h1>
-        <div className='steps'>
-          <button onClick={menos}><img src={flechai}/></button>
-          <h1 className='index'>{index}</h1>
-          <button onClick={mas}><img src={flechad}/></button>
-        </div>
-        <div className='change'>
-          <button onClick={restar}><img src={flechai}/></button>
-          <h2>{index1}</h2>
-          <button onClick={sumar}><img src={flechad}/></button>
-        </div> 
-        <p>{fecha.format('DD-MM-YYYY')}</p>
-        <p>{horaActual.format('hh:mm:ss')}</p>
-        <img src={urlImg} className="App-logo" />
-      </header>
+      <h1>Bienvenido, {nombreIngresado || 'Invitado'}</h1>
+      <button onClick={mostrarFormularioHandler}>Mostrar Formulario</button>
+      {mostrarFormulario && (
+        <NameForm onSubmit={manejarFormulario} onClose={cerrarFormularioHandler} />
+      )}
     </div>
   );
-  }
+};
 
-
+export default App;
---
+++ desafio2/src/PopupForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const PopupForm = ({ onClose }) => {
+  const [nombre, setNombre] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Puedes hacer algo con el nombre ingresado aquí
+    console.log('Nombre ingresado:', nombre);
+    onClose();
+  };
+
+  return (
+    <div className="popup">
+      <div className="popup-content">
+        <h2>Ingrese su nombre</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+            required
+          />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default PopupForm;
